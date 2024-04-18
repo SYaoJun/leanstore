f56c6d652882336b69220728c2552eeac69e71cb
yaojun <940334249@qq.com>
2024-04-18 10:21:00 +0800

    can run on ubuntu 22.04 x86-64

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# 2-Tree
+1. tcmalloc链接有点问题，我做的全路径链接
+2. rocksdb要使用zxj自己仓库中的版本
+## 编译
+```sh
+make -j 4  > make.log 2>&1
+```
+
 # TieredIndexing
 Tiered Indexing is a general way to improve the memory utilization of buffer-managed data structures including B+tree, Hashing, Heap, and Log-Structured-Merge Tree. It is generalization of [2-Tree](https://www.cidrdb.org/cidr2023/papers/p57-zhou.pdf). The philosophy of Tiered Indexing is to maintain a hierarchy of homogeneous index structures with different hotness that share a buffer pool. Tiered Indexing actively performs efficient inter-tier record migration based on record hotness. 
 
---
+++ libs/rocksdb.cmake
@@ -0,0 +1,36 @@
+# ---------------------------------------------------------------------------
+# leanstore
+# ---------------------------------------------------------------------------
+# Note: RocksDB has CMake build only for windows 64-bit, therefore we use make after cloning their repo
+# Not used atm, use sudo apt install -y librocksdb-dev instead
+
+include(ExternalProject)
+find_package(Git REQUIRED)
+
+# Get rocksdb
+ExternalProject_Add(
+        rocksdb_src
+        PREFIX "vendor/rocksdb"
+        GIT_REPOSITORY "https://github.com/zxjcarrot/rocksdb-7.1.1.git"
+        GIT_TAG master
+        GIT_SHALLOW TRUE
+        TIMEOUT 10
+        CONFIGURE_COMMAND ""
+        UPDATE_COMMAND ""
+        INSTALL_COMMAND ""
+        BUILD_COMMAND $(MAKE) static_lib
+        BUILD_IN_SOURCE TRUE
+)
+
+# Prepare rocksdb
+ExternalProject_Get_Property(rocksdb_src source_dir)
+set(ROCKSDB_INCLUDE_DIR ${source_dir}/include)
+set(ROCKSDB_LIBRARY_PATH ${source_dir}/librocksdb.a)
+#set(ROCKSDB_LIBRARY_PATH ${source_dir}/librocksdb.so)
+file(MAKE_DIRECTORY ${ROCKSDB_INCLUDE_DIR})
+add_library(rocksdb STATIC IMPORTED)
+set_property(TARGET rocksdb PROPERTY IMPORTED_LOCATION ${ROCKSDB_LIBRARY_PATH})
+set_property(TARGET rocksdb APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${ROCKSDB_INCLUDE_DIR})
+
+# Dependencies
+add_dependencies(rocksdb rocksdb_src)
